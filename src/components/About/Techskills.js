31b5a160b10699215875c2e38646cce910641239
Dane Cronin <danecronin@gmail.com>
2022-12-11 22:18:06 -0700

    added tech skills logos to about section, needs more styling

---
+++ src/components/About/Techskills.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+//import icons for tech skills
+import {
+    DiJavascript1,
+    DiReact,
+    DiNodejs,
+    DiMongodb,
+    DiGit,
+  } from "react-icons/di";
+
+  function TechSkill () {
+
+    return (
+        <div style={{ justifyContent: "center", paddingBottom: "50px" }}>
+        
+          <div xs={4} md={2} className="tech-icons">
+            <DiJavascript1 />
+          </div>
+          <div xs={4} md={2} className="tech-icons">
+            <DiNodejs />
+          </div>
+          <div xs={4} md={2} className="tech-icons">
+            <DiReact />
+          </div>
+          <div xs={4} md={2} className="tech-icons">
+            <DiMongodb />
+          </div>
+         
+          <div xs={4} md={2} className="tech-icons">
+            <DiGit />
+          </div>
+        
+        </div>
+      );
+  };
+
+  export default TechSkill;
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TechSkill from './Techskills';
 
 //Import images to use in About section
 import CroninDane from '../../assets/images/CroninDane.jpg';
@@ -6,32 +7,37 @@ import CroninDane from '../../assets/images/CroninDane.jpg';
 const About = () => {
     return (
         <section id='about'>
-        <div name="about" className= "max-w-screen-lg p-4 mb-8 mt-10 mx-auto flex flex-col justify-content-center" style = {{height:"100vh"}}>
-         <div>
-         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4">
-           About
-          </p>
-        </div>
-            <div>
-            <img src={CroninDane} className="my-2" style={{ width: "25%" }} alt="profile avatar of Dane Cronin" />
-               
-            </div>
-            <div>
-            <p> 
-            Dane Cronin is a professional photographer based in Boulder, Colorado specializing in cycling photography, architectural,
-          and interior design photography. He is currently enrolled in a full-stack web development program through the
-          University of North Carolina, Charlotte working to add to his skillset.
-            </p>
-            </div>
+            <div name="about" className="max-w-screen-lg p-4 mb-8 mt-10 mx-auto flex flex-col justify-content-center" style={{ height: "100vh" }}>
+                <div>
+                    <div className="pb-8">
+                        <p className="text-4xl font-bold inline border-b-4">
+                            About
+                        </p>
+                    </div>
+                    <div>
+                        <img src={CroninDane} className="my-2" style={{ width: "25%" }} alt="profile avatar of Dane Cronin" />
 
+                    </div>
+                    <div>
+                        <p>
+                            Dane Cronin is a professional photographer based in Boulder, Colorado specializing in cycling photography, architectural,
+                            and interior design photography. He is currently enrolled in a full-stack web development program through the
+                            University of North Carolina, Charlotte working to add to his skillset.
+                        </p>
+                    </div>
+                </div>
+                <div>
             
+        <h1 className="project-heading">
+          Professional <strong className="purple">Skillset </strong>
+        </h1>
 
+        <TechSkill/>
 
+                </div>
             </div>
-        </div>
         </section>
-        
+
     );
 };
 
